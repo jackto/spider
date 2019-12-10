bf8c1c489e815d1a3e07fbdf068c456e08b56915
chenxinhui <chenxinhui@licaigc.com>
2019-12-10 16:21:05 +0800

    init

---
+++ setup.py
@@ -0,0 +1,11 @@
+# coding=utf-8
+from setuptools import setup
+import setuptools
+
+setup(
+
+	name ='douyin-spider-simple',
+	version ='0.0.1',
+	author ='brian',
+	packages = setuptools.find_packages(),
+)
